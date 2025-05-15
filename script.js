2cd91635f1befc2bc42929b69ca365160ac9b85c
Karan S Khedkar <103458258+karanskhedkar@users.noreply.github.com>
2025-05-15 17:14:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 function stringChop(str, size) {
   // your code here
+  const chunks=[];
+  if(str){
   size=Number(size);
   let len=str.length;
   let i=0;
-  const chunks=[];
   while(i<len){
       chunks.push(str.slice(i,i+size));
       i+=size;
+  }  
   }
   return chunks;
 }
